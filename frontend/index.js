09d90afbbb1f3d41a06a1bd9a4451c3c8c027505
Ford-Grable <fordgrable.12@gmail.com>
2024-07-10 03:34:19 -0400

    Printergit add .git add .!

---
+++ frontend/index.js
@@ -0,0 +1,72 @@
+
+
+$("#callsign-request").submit(function() {
+    $.post("/printCallsign", { callsign: $(this).find("input[name='cs']").val() })
+    .done(async function(data) {
+        if(data === "callsign not found") {
+            $("#not-found").html("Callsign not found");
+            setTimeout(() => {
+                $("#not-found").html("");
+            }, 5000);
+            return;
+        }
+        qz.websocket.connect().then(() => {
+            var config = qz.configs.create("Boca BIDI FGL 26/46 300 DPI");
+            return qz.print(config, getData(data))
+        }).then(() => {
+            return qz.websocket.disconnect();
+        }).catch((err) => {
+            console.error(err);
+            // process.exit(1);
+        });
+    });
+});
+
+setInterval(() => {
+    $.post("/getNewPlans")
+    .done(function(data) {
+        if(data.length == 0) return;
+        qz.websocket.connect().then(async() => {
+            var config = qz.configs.create("Boca BIDI FGL 26/46 300 DPI");
+            for(let i = 0; i < data.length; i++) {
+                await qz.print(config, getData(data[i]));
+            }
+            return;
+        }).then(() => {
+            return qz.websocket.disconnect();
+        }).catch((err) => {
+            console.error(err);
+            // process.exit(1);
+        });
+    });
+}, 15000);
+
+function getData(data) {
+    return [
+        `<RC10,10><TTF1,12><HW1,1>${data[0]}`,
+        `<RC60,130><TTF1,12><HW1,1>${data[1] ? data[1] : " "}`,
+        `<RC60,300><TTF1,24><HW1,1>${data[2]}`,
+        `<RC120,10><TTF1,12><HW1,1>${data[3]}`,
+        `<RC225,10><TTF1,12><HW1,1>${data[4]}`,
+        `<RC0,455><LT4><VX375>`,
+        `<RC0,465><TTF1,12><HW1,1>${data[5]}`,
+        `<RC90,455><LT4><HX185>`,
+        `<RC110,465><TTF1,12><HW1,1>${data[6]}`,
+        `<RC190,455><LT4><HX185>`,
+        `<RC220,465><TTF1,12><HW1,1>${data[7]}`,
+        `<RC0,635><LT4><VX375>`,
+        `<RC0,690><TTF1,12><HW1,1>KATL`,
+        `<RC0,915><LT4><VX375>`,
+        `<RC0,935><TTF1,12><HW1,1>${data[12] ? data[8] : data[8] + " " + data[9]}`,
+        `<RC120,935><TTF1,12><HW1,1>${data[12] ? data[9] : ""}`,
+        `<RC226,935><F10><HW1,1>${data[13] ? "o" : " "}`,
+        `<RC220,955><TTF1,12><HW1,1>${data[10]}`,
+        `<RC0,1850><LT4><VX375>`,
+        `<RC0,2050><LT4><VX375>`,
+        `<RC0,2250><LT4><VX375>`,
+        `<RC90,1850><LT4><HX900>`,
+        `<RC190,1850><LT4><HX900>`,
+        `<RC200,190><X2><NP10>*${data[11]}*`,
+        `<p>`
+    ];
+}
---
+++ index.js
@@ -0,0 +1,194 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import * as fs from "fs";
+
+import axios from "axios";
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/frontend", express.static(path.resolve("frontend")));
+
+app.get("*", (req, res) => {
+    return res.sendFile(path.resolve("./frontend/index.html"));
+});
+
+app.post("/getNewPlans", async(req, res) => {
+    const atlDepartures = (await axios.get("https://data.vatsim.net/v3/vatsim-data.json")).data["pilots"].filter((pilot) => pilot["flight_plan"] !== null && pilot["flight_plan"].departure == "KATL" && isAtATL(pilot["latitude"], pilot["longitude"]));
+    const stripsToPrint = checkForPrintedStrips(atlDepartures);
+    let formattedStrips = [];
+
+    for(let i = 0; i < stripsToPrint.length; i++) {
+        let strip = stripsToPrint[i];
+        formattedStrips.push([
+            strip["callsign"], //0
+            strip["flight_plan"].revision_id > 1 ? strip["flight_plan"].revision_id - 1 : false, //1
+            getGate(formatRoute(strip["flight_plan"].route)[0]), //2
+            formatAcType(strip["flight_plan"].aircraft_faa), //3
+            getCID(strip["flight_plan"].remarks), //4
+            strip["flight_plan"].assigned_transponder, //5
+            "P" + strip["flight_plan"].deptime, //6
+            formatAltitude(strip["flight_plan"].altitude), //7
+            "KATL " + formatRoute(strip["flight_plan"].route)[0], //8
+            strip["flight_plan"].arrival, //9
+            " " + formatRemarks(strip["flight_plan"].remarks)[0], //10
+            strip["cid"], //11
+            formatRoute(strip["flight_plan"].route)[1], //12 (2nd line arrival?)
+            formatRemarks(strip["flight_plan"].remarks)[1] //13
+        ]);
+    }
+
+    return res.send(formattedStrips);
+});
+
+app.post("/printCallsign", async(req, res) => {
+    let strip = (await axios.get("https://data.vatsim.net/v3/vatsim-data.json")).data["pilots"].filter((pilot) => pilot["flight_plan"] !== null && pilot["callsign"] == req.body.callsign)[0];
+    if(strip.length === 0) return res.send("callsign not found")
+    const formattedStrip = [
+        strip["callsign"], //0
+        strip["flight_plan"].revision_id > 1 ? strip["flight_plan"].revision_id - 1 : false, //1
+        getGate(formatRoute(strip["flight_plan"].route)[0]), //2
+        formatAcType(strip["flight_plan"].aircraft_faa), //3
+        getCID(strip["flight_plan"].remarks), //4
+        strip["flight_plan"].assigned_transponder, //5
+        "P" + strip["flight_plan"].deptime, //6
+        formatAltitude(strip["flight_plan"].altitude), //7
+        "KATL " + formatRoute(strip["flight_plan"].route)[0], //8
+        strip["flight_plan"].arrival, //9
+        " " + formatRemarks(strip["flight_plan"].remarks)[0], //10
+        strip["cid"], //11
+        formatRoute(strip["flight_plan"].route)[1], //12 (2nd line arrival?)
+        formatRemarks(strip["flight_plan"].remarks)[1] //13
+    ];
+
+    return res.send(formattedStrip);
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
+
+const getCID = (remarks) => {
+    remarks = remarks.toLowerCase();
+    let r1 = Math.floor(Math.random() * 10);
+    let r2 = Math.floor(Math.random() * 10);
+    let r3 = Math.floor(Math.random() * 10);
+
+    if(remarks.includes("blind") || remarks.includes("vision")) r2 = "B";
+    if(remarks.includes("/t/")) r3 = "T";
+    if(remarks.includes("/r/")) r3 = "R";
+
+    return `${r1}${r2}${r3}`;
+}
+
+const checkForPrintedStrips = (atlDepartures) => {
+    const printedStrips = JSON.parse(fs.readFileSync("./printed_strips.json"));
+    const newStrips = atlDepartures.map(plan => plan["callsign"]);
+
+    //Check for new data with different revision_id
+    const updatedStrips = atlDepartures.filter(plan => {
+        const printedStrip = printedStrips.find(printedStrip => printedStrip["callsign"] == plan["callsign"]);
+        return !printedStrip || printedStrip["flight_plan"].revision_id !== plan["flight_plan"].revision_id;
+    });
+
+    //Filter printed strips to keep up-to-date data
+    const final = printedStrips.filter(printedStrip => newStrips.includes(printedStrip["callsign"]))
+    .map(printedStrip => {
+        const newStrip = atlDepartures.find(strip => strip["callsign"] == printedStrip["callsign"]);
+        return newStrip ? { ...printedStrip, flight_plan: { ...printedStrip["flight_plan"], revision_id: newStrip["flight_plan"].revision_id } } : printedStrip;
+    });
+    final.push(...updatedStrips.filter(strip => !final.some(printedStrip => printedStrip["callsign"] == strip["callsign"])));
+
+    fs.writeFileSync("./printed_strips.json", JSON.stringify(final, null, 2));
+
+    return updatedStrips;
+}
+
+const getGate = (route) => {
+    const gates = {
+        "PENCL": "N3",
+        "VARNM": "N5",
+        "PADGT": "N4",
+        "SMKEY": "N6",
+
+        "PLMMR": "E3",
+        "JACCC": "E5",
+        "PHIIL": "E4",
+        "GAIRY": "E6",
+
+        "VRSTY": "S3",
+        "SMLTZ": "S5",
+        "BANNG": "S4",
+        "HAALO": "S6",
+
+        "POUNC": "W3",
+        "KAJIN": "W5",
+        "NASSA": "W4",
+        "CUTTN": "W6"
+    }
+
+    let sid = route.split(" ")[0].substring(0,5);
+    return gates[sid] ?? false;
+}
+
+const formatRoute = (route) => {
+    route = route.toUpperCase().replace("DCT", "")
+    route = route.replace("KATL", "")
+    route = route.trim();
+    route = route.replace(".", " ");
+    route = route.split(" ").map(a => {
+        if(a.includes("/")) return a.split("/")[0];
+        return a;
+    }).join(" ");
+    let tooBig = route.split(" ");
+    if(tooBig.length >= 4) route = tooBig.slice(0,3).join(" ") + " . / ."
+
+    return [route, tooBig.length >= 4 ? true : false];
+}
+
+const formatRemarks = (remarks) => {
+    remarks = remarks.replace("  ", " ");
+    remarks = remarks.replace("/V/", "");
+    remarks = remarks.replace("/T/", "");
+    remarks = remarks.replace("/R/", "");
+    if(remarks.trim().length == 0) return "";
+
+    remarks = remarks.split("RMK/")[1];
+
+    if(remarks.length > 25) remarks = remarks.substring(0,25).slice(0, -3) + "***";
+    remarks = remarks.trim();
+
+    return [remarks, remarks.length > 0 ? true : false];
+}
+
+const formatAltitude = (altitude) => {
+    let formatted = Math.floor(altitude / 100).toString();
+    while (formatted.length < 3) formatted = "0" + formatted;
+
+    return formatted;
+}
+
+const formatAcType = (acType) => {
+    acType = acType.replace("H/", "");
+    acType = acType.replace("J/", "");
+
+    acType = acType + "/";
+    acType = JSON.parse(fs.readFileSync("./acft_database.json"))["aircraft"][acType.toUpperCase().split("/")[0]].recat + "/" + acType;
+
+    return acType.slice(0,-1);
+}
+
+const isAtATL = (lat, long) => {
+    const fence = 0.026079;
+
+    const northernLat = lat + fence;
+    const westernLong = long - fence;
+
+    const southernLat = lat - fence;
+    const easternLong = long - fence;
+
+    if((lat < northernLat && lat > southernLat) && (long > westernLong && long < easternLong)) return true;
+}
